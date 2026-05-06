d981c4a65561141d67763a0588d9c2fc2d32cb95
Magdalena Zydorczak <s253712@student.dtu.dk>
2026-05-06 10:38:38 +0200

    Added stability assessment script

---
+++ src/stability_assessment.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+##########################
+## Stability assessment ##
+##########################
+
+# Working directory: src
+
+# Set up directories
+mkdir ../stability_assessment
+cd ../stability_assessment
+
+# Activate conda environment with necessary tools (naccess, mutatex)
+conda activate /home/ctools/protein_structure_course
+
+# Calculate the atomic accessible area with Naccess
+naccess ../data/structure_selection/AF-Q13188-F1-model_v6.pdb
+
+# Run MutateX to calculate the ddG values for the mutations listed in mutation_list.txt
+nohup mutatex ../data/structure_selection/AF-Q13188-F1-model_v6.pdb -p 1 -m ../data/foldxsuite5_templates/mutation_list.txt \
+    -x /home/ctools/foldx/foldx -f suite5 -R ../data/foldxsuite5_templates/repair_runfile_template.txt \
+    -M ../data/foldxsuite5_templates/mutate_runfile_template.txt -q poslist.txt -L -l -v -C none &
+
+# Summarize MutateX results and convert to CSV format
+ddg2excel -p ../data/structure_selection/AF-Q13188-F1-model_v6.pdb -l ../data/foldxsuite5_templates/mutation_list.txt \
+    -q poslist.txt -d results/mutation_ddgs/AF-Q13188-F1-model_v6_model0_checked_Repair -F csv
+
+# Generate a heatmap of the ddG values
+ddg2heatmap -p ../data/structure_selection/AF-Q13188-F1-model_v6.pdb -l ../data/foldxsuite5_templates/mutation_list.txt \
+    -q poslist.txt -d results/mutation_ddgs/final_averages/
